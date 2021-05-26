953ee94e96f32243418a0706179eb8329009201a
thiago_mourao <thiago.mourao.ifmg@gmail.com>
2021-05-26 20:18:25 -0300

    Oberver design pattern to update tableview

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -5,11 +5,12 @@ import java.io.Serializable;
 public class Department implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	
 	private Integer id;
 	private String name;
 	
 	public Department() {
+		
 	}
 
 	public Department(Integer id, String name) {
@@ -23,14 +24,6 @@ public class Department implements Serializable {
 
 	public void setId(Integer id) {
 		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 	@Override
@@ -58,8 +51,17 @@ public class Department implements Serializable {
 		return true;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	@Override
 	public String toString() {
 		return "Department [id=" + id + ", name=" + name + "]";
 	}
+	
 }
